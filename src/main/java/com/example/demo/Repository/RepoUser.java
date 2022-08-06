f3b373941cdc8872c4a094bc4295f6fd9c3ac944
lavelazquez <lavelazquez@frba.utn.edu.ar>
2022-08-06 18:50:30 -0300

    change RestResource

--- src/main/java/com/example/demo/Repository/RepoUser.java
+++ src/main/java/com/example/demo/Repository/RepoUser.java
@@ -3,9 +3,10 @@ package com.example.demo.Repository;
 import com.example.demo.Entity.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 
-@RepositoryRestResource(path = "user")
+@RestResource(path = "user")
 public interface RepoUser extends PagingAndSortingRepository<User,Integer> {
     // public User findUserByNameUserAndPassword(String nameUser, String password);
 }
