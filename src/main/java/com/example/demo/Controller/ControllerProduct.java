66b8a5a77229e42256d07ed06e88a5c7ad93a9be
lavelazquez <lavelazquez@frba.utn.edu.ar>
2022-08-06 19:08:59 -0300

    addGetUsers()

--- src/main/java/com/example/demo/Controller/ControllerProduct.java
+++ src/main/java/com/example/demo/Controller/ControllerProduct.java
@@ -1,66 +1,66 @@
-//package com.example.demo.Controller;
-//
-//import com.example.demo.Entity.Product;
-//import com.example.demo.Repository.RepoBrand;
-//import com.example.demo.Repository.RepoCategory;
-//import com.example.demo.Repository.RepoProduct;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.rest.webmvc.RepositoryRestController;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Optional;
-//
-//
-//@RepositoryRestController
-//@CrossOrigin
-//public class ControllerProduct {
-//
-//    @Autowired
-//    private RepoProduct repoProduct;
-//    @Autowired
-//    private RepoBrand repoBrand;
-//    @Autowired
-//    private RepoCategory repoCategory;
-//
-//    @DeleteMapping(path = "/product/{id}")
-//    public @ResponseBody ResponseEntity<Product> deleteProduct(@PathVariable("id") Integer id){
-//        Optional<Product> product = repoProduct.findById(id);
-//        if (product.isPresent()){
-//            product.get().setStock(0);
-//            this.repoProduct.save(product.get());
-//            return ResponseEntity.ok(product.get());
-//        }
-//        else{
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
-//
-//    @PutMapping(path = "/product/{id}")
-//    public @ResponseBody ResponseEntity<Product> updateProduct(@PathVariable("id") Integer id, @RequestBody Product p){
-//        Optional<Product> product = repoProduct.findById(id);
-//        if (product.isPresent()){
-//            Product p2 = product.get();
-//            p2.setNameProduct(p.getNameProduct());
-//            p2.setStock(p.getStock());
-//            p2.setPrice(p.getPrice());
-//            p2.setBrand(repoBrand.findById(p.getBrandId()).get());
-//            p2.setCategory(repoCategory.findById(p.getCategoryId()).get());
-//            this.repoProduct.save(p2);
-//            return ResponseEntity.ok(p2);
-//        }
-//        else{
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
-//
-//    @PostMapping(path = "/product")
-//    public @ResponseBody ResponseEntity<Product> updateProduct(@RequestBody Product p){
-//        p.setBrand(repoBrand.findById(p.getBrandId()).get());
-//        p.setCategory(repoCategory.findById(p.getCategoryId()).get());
-//        this.repoProduct.save(p);
-//        return ResponseEntity.ok(p);
-//    }
-//
-//
-//}
+package com.example.demo.Controller;
+
+import com.example.demo.Entity.Product;
+import com.example.demo.Repository.RepoBrand;
+import com.example.demo.Repository.RepoCategory;
+import com.example.demo.Repository.RepoProduct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+
+@RepositoryRestController
+@CrossOrigin
+public class ControllerProduct {
+
+    @Autowired
+    private RepoProduct repoProduct;
+    @Autowired
+    private RepoBrand repoBrand;
+    @Autowired
+    private RepoCategory repoCategory;
+
+    @DeleteMapping(path = "/product/{id}")
+    public @ResponseBody ResponseEntity<Product> deleteProduct(@PathVariable("id") Integer id){
+        Optional<Product> product = repoProduct.findById(id);
+        if (product.isPresent()){
+            product.get().setStock(0);
+            this.repoProduct.save(product.get());
+            return ResponseEntity.ok(product.get());
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping(path = "/product/{id}")
+    public @ResponseBody ResponseEntity<Product> updateProduct(@PathVariable("id") Integer id, @RequestBody Product p){
+        Optional<Product> product = repoProduct.findById(id);
+        if (product.isPresent()){
+            Product p2 = product.get();
+            p2.setNameProduct(p.getNameProduct());
+            p2.setStock(p.getStock());
+            p2.setPrice(p.getPrice());
+            p2.setBrand(repoBrand.findById(p.getBrandId()).get());
+            p2.setCategory(repoCategory.findById(p.getCategoryId()).get());
+            this.repoProduct.save(p2);
+            return ResponseEntity.ok(p2);
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping(path = "/product")
+    public @ResponseBody ResponseEntity<Product> updateProduct(@RequestBody Product p){
+        p.setBrand(repoBrand.findById(p.getBrandId()).get());
+        p.setCategory(repoCategory.findById(p.getCategoryId()).get());
+        this.repoProduct.save(p);
+        return ResponseEntity.ok(p);
+    }
+
+
+}
--- src/main/java/com/example/demo/Repository/RepoUser.java
+++ src/main/java/com/example/demo/Repository/RepoUser.java
@@ -3,11 +3,8 @@ package com.example.demo.Repository;
 import com.example.demo.Entity.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 
-@RepositoryRestController(path = "user")
 public interface RepoUser extends PagingAndSortingRepository<User,Integer> {
     // public User findUserByNameUserAndPassword(String nameUser, String password);
 }
