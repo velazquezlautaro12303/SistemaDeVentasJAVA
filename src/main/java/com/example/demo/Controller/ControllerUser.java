bc1763fbbad5f15f851b2c36817d0970f84c5a0d
lavelazquez <lavelazquez@frba.utn.edu.ar>
2022-08-06 17:48:07 -0300

    comente los controllers

--- src/main/java/com/example/demo/Controller/ControllerUser.java
+++ src/main/java/com/example/demo/Controller/ControllerUser.java
@@ -1,33 +1,33 @@
-package com.example.demo.Controller;
-
-import com.example.demo.Entity.User;
-import com.example.demo.Repository.RepoUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
-
-@RepositoryRestController
-@CrossOrigin
-public class ControllerUser {
-
-    @Autowired
-    private RepoUser user;
-
-    @DeleteMapping(path = "user/{id}")
-    public @ResponseBody ResponseEntity<User> updateUser(@PathVariable("id") Integer id){
-        Optional<User> userOptional = this.user.findById(id);
-        if (userOptional.isPresent()){
-            User user = userOptional.get();
-            user.setAvailable(Boolean.FALSE);
-            this.user.save(user);
-            return ResponseEntity.ok(user);
-        }
-        else{
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-}
+//package com.example.demo.Controller;
+//
+//import com.example.demo.Entity.User;
+//import com.example.demo.Repository.RepoUser;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.rest.webmvc.RepositoryRestController;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Optional;
+//
+//@RepositoryRestController
+//@CrossOrigin
+//public class ControllerUser {
+//
+//    @Autowired
+//    private RepoUser user;
+//
+//    @DeleteMapping(path = "user/{id}")
+//    public @ResponseBody ResponseEntity<User> updateUser(@PathVariable("id") Integer id){
+//        Optional<User> userOptional = this.user.findById(id);
+//        if (userOptional.isPresent()){
+//            User user = userOptional.get();
+//            user.setAvailable(Boolean.FALSE);
+//            this.user.save(user);
+//            return ResponseEntity.ok(user);
+//        }
+//        else{
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+//
+//}
