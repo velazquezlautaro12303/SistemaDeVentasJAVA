3952877d0b3f769b172a4014ea1281de393f5285
lavelazquez <lavelazquez@frba.utn.edu.ar>
2022-08-06 18:18:18 -0300

    replace RepositoryRestResource by RestResource

--- src/main/java/com/example/demo/Repository/RepoProduct.java
+++ src/main/java/com/example/demo/Repository/RepoProduct.java
@@ -8,7 +8,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(path = "product")
+@RestResource(path = "product")
+//@RepositoryRestResource(path = )
 @CrossOrigin
 public interface RepoProduct extends PagingAndSortingRepository<Product,Integer> {
 
