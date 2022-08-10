ccbb1ac31cae69ced170fcee2b863b04ae2649b8
lavelazquez <lavelazquez@frba.utn.edu.ar>
2022-08-10 19:16:28 -0300

    funcionando

--- src/main/java/com/example/demo/Repository/RepoUser.java
+++ src/main/java/com/example/demo/Repository/RepoUser.java
@@ -2,9 +2,6 @@ package com.example.demo.Repository;
 
 import com.example.demo.Entity.User;
 import com.example.demo.View.UserView;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
