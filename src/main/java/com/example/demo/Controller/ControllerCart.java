fea8f0421cc71193164baf160a8562626eeed31b
lavelazquez <lavelazquez@frba.utn.edu.ar>
2022-08-10 19:08:07 -0300

    funcionando

--- src/main/java/com/example/demo/Controller/ControllerCart.java
+++ src/main/java/com/example/demo/Controller/ControllerCart.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RepositoryRestController
-@RequestMapping(path = "")
 @CrossOrigin
 public class ControllerCart {
 
